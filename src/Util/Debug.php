29756c108a161c4435ae35a154b5e468193e9c10
Ansas <mail@dhm80.de>
2016-08-29 21:54:57 +0200

    Added Util\Debug class

---
+++ src/Util/Debug.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * This file is part of the PHP components.
+ *
+ * For the full copyright and license information, please view the LICENSE.md file distributed with this source code.
+ *
+ * @license MIT License
+ * @link    https://github.com/ansas/php-component
+ */
+
+namespace Ansas\Util;
+
+/**
+ * Class Debug
+ *
+ * @package Ansas\Util
+ * @author  Ansas Meyer <mail@ansas-meyer.de>
+ */
+class Debug
+{
+    /**
+     * Print variable.
+     *
+     * @param      $data
+     * @param null $description
+     */
+    public static function print($data, $description = null)
+    {
+        if (self::isCli()) {
+            if ($description) {
+                echo $description . ":\n";
+            }
+            print_r($data);
+            if (is_scalar($data)) {
+                echo "\n\n";
+            }
+        } else {
+            echo "<pre>\n";
+            if ($description) {
+                echo $description . ":\n";
+            }
+            print_r($data);
+            echo "</pre>\n";
+        }
+    }
+
+    /**
+     * Print separator.
+     *
+     * @param string $char
+     * @param int    $repeat
+     */
+    public static function separator($char = '=', $repeat = 50)
+    {
+        echo str_repeat($char, $repeat) . (self::isCli() ? "\n" : "<br>\n");
+    }
+
+    /**
+     * Check if running in cli modus.
+     *
+     * @return bool
+     */
+    public static function isCli()
+    {
+        return PHP_SAPI === 'cli-server';
+    }
+}
