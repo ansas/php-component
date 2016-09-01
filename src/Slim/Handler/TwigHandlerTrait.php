70221472b77e7ea6ba3acbdaa27237e1e1226c65
Ansas <mail@dhm80.de>
2016-09-02 01:15:03 +0200

    Updated documentation

--- src/Slim/Handler/TwigHandlerTrait.php
+++ src/Slim/Handler/TwigHandlerTrait.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the PHP components.
+ *
+ * For the full copyright and license information, please view the LICENSE.md file distributed with this source code.
+ *
+ * @license MIT License
+ * @link    https://github.com/ansas/php-component
+ */
 
 namespace Ansas\Slim\Handler;
 
