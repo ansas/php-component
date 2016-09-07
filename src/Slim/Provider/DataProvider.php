91148eb3e9f7d00107b2bd872effa68cfc1b49d1
Ansas <mail@dhm80.de>
2016-09-07 09:36:15 +0200

    Extended DataProvider

--- src/Slim/Provider/DataProvider.php
+++ src/Slim/Provider/DataProvider.php
@@ -22,17 +22,55 @@ use Pimple\Container;
 class DataProvider extends AbstractProvider
 {
     /**
+     * @var null|string[] Collections to register.
+     */
+    protected $collections;
+
+    /**
+     * DataProvider constructor.
+     *
+     * @param null|string|string[] $collections [optional] Collections to register.
+     */
+    public function __construct($collections = null)
+    {
+        $this->setCollections($collections);
+    }
+
+    /**
      * {@inheritDoc}
      */
     public function register(Container $container)
     {
-        /**
-         * Add dependency (DI).
-         *
-         * @return Collection
-         */
-        $container['data'] = function () {
-            return new Collection();
-        };
+        // Set 'data' as default collection container that will always be created and add individual collections
+        $collections = ['data'];
+        if ($this->collections) {
+            $collections = array_merge($collections, $this->collections);
+            $collections = array_unique($collections);
+        }
+
+        foreach ($collections as $collection) {
+            /**
+             * Add dependency (DI).
+             *
+             * @return Collection
+             */
+            $container[$collection] = function () {
+                return new Collection();
+            };
+        }
+    }
+
+    /**
+     * Set collections to create on container registration.
+     *
+     * @param null|string|string[] $collections [optional] Collections to register.
+     *
+     * @return $this
+     */
+    public function setCollections($collections)
+    {
+        $this->collections = $collections ? (array) $collections : null;
+
+        return $this;
     }
 }
