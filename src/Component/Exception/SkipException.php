64e7523ba044af0479acd862ab48e0ec66c23dc4
Ansas <mail@dhm80.de>
2017-01-13 16:29:26 +0100

    Added Exceptions

---
+++ src/Component/Exception/SkipException.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of the PHP components.
+ *
+ * For the full copyright and license information, please view the LICENSE.md file distributed with this source code.
+ *
+ * @license MIT License
+ * @link    https://github.com/ansas/php-component
+ */
+
+namespace Ansas\Component\Exception;
+
+use Exception;
+
+/**
+ * Class SkipException
+ *
+ * @package Ansas\Component\Exception
+ * @author  Ansas Meyer <mail@ansas-meyer.de>
+ */
+class SkipException extends Exception
+{
+
+}
