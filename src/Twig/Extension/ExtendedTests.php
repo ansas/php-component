1060a43ecc4be9a23f6cc1b34e27bf9614004c9a
Ansas <mail@dhm80.de>
2017-12-09 10:51:43 +0100

    Added twig extension ExtendedTests

---
+++ src/Twig/Extension/ExtendedTests.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of the PHP components.
+ *
+ * For the full copyright and license information, please view the LICENSE.md file distributed with this source code.
+ *
+ * @license MIT License
+ * @link    https://github.com/ansas/php-component
+ */
+
+namespace Ansas\Twig\Extension;
+
+use Twig_Extension;
+use Twig_SimpleTest;
+
+/**
+ * Class ExtendedTests
+ *
+ * @package Ansas\Twig\Extension
+ * @author  Ansas Meyer <mail@ansas-meyer.de>
+ */
+class ExtendedTests extends Twig_Extension
+{
+    public function getTests()
+    {
+        $filters = [
+            new Twig_SimpleTest('instanceof', [$this, '_instanceof']),
+        ];
+
+        return $filters;
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'extendedTests';
+    }
+
+    function _instanceof($var, $instance)
+    {
+        return $var instanceof $instance;
+    }
+}
