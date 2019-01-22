1eb8f931ade6bd3fc9c7dae7cf1ecbda5331afdf
Ansas <mail@dhm80.de>
2019-01-22 17:51:54 +0100

    Changed ExtendedRequest::getHostAce() and getHostIdn() variants (considering ß now)

--- src/Slim/Http/ExtendedRequest.php
+++ src/Slim/Http/ExtendedRequest.php
@@ -137,7 +137,7 @@ class ExtendedRequest extends Request
      */
     public function getHostAce()
     {
-        return idn_to_ascii($this->getHost(), IDNA_DEFAULT, INTL_IDNA_VARIANT_UTS46);
+        return idn_to_ascii($this->getHost(), IDNA_NONTRANSITIONAL_TO_ASCII, INTL_IDNA_VARIANT_UTS46);
     }
 
     /**
@@ -149,7 +149,7 @@ class ExtendedRequest extends Request
      */
     public function getHostIdn()
     {
-        return idn_to_utf8($this->getHost(), IDNA_DEFAULT, INTL_IDNA_VARIANT_UTS46);
+        return idn_to_utf8($this->getHost(), IDNA_NONTRANSITIONAL_TO_UNICODE, INTL_IDNA_VARIANT_UTS46);
     }
 
     /**
