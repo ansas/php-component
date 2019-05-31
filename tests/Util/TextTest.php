a271f1fc9329fd8a4abf2074bdc086d9d395ea82
Ansas <mail@dhm80.de>
2019-05-31 11:24:17 +0200

    Bugfix in test classes (swapped file names)

--- tests/Util/TextTest.php
+++ tests/Util/TextTest.php
@@ -13,76 +13,256 @@ namespace Ansas\Util;
 use PHPUnit\Framework\TestCase;
 
 /**
- * Class PathTest
+ * Class TextTest
  *
  * @package Ansas\Util
  * @author  Ansas Meyer <mail@ansas-meyer.de>
  */
-class PathTest extends TestCase
+class TextTest extends TestCase
 {
-    public function testFromCamelCase()
+    public function testMaxCharWidth()
     {
         $this->assertEquals(
-            '',
-            Path::fromCamelCase('')
+            0,
+            Text::maxCharWidth('')
         );
 
         $this->assertEquals(
-            'abc',
-            Path::fromCamelCase('abc')
+            1,
+            Text::maxCharWidth(' ')
         );
 
         $this->assertEquals(
-            'go/to/path',
-            Path::fromCamelCase('goToPath')
+            1,
+            Text::maxCharWidth('abc')
         );
 
         $this->assertEquals(
-            '/go/to/path',
-            Path::fromCamelCase('GoToPath')
+            2,
+            Text::maxCharWidth('äöüß')
         );
 
         $this->assertEquals(
-            'a/b/c',
-            Path::fromCamelCase('aBC')
+            3,
+            Text::maxCharWidth('€')
         );
 
         $this->assertEquals(
-            '/a/b/c',
-            Path::fromCamelCase('ABC')
+            3,
+            Text::maxCharWidth('Übrig: 123,56 €')
+        );
+
+        $this->assertEquals(
+            4,
+            Text::maxCharWidth('🌟')
         );
     }
 
-    public function testToCamelCase()
+    public function testStripEmails()
     {
         $this->assertEquals(
             '',
-            Path::toCamelCase('')
+            Text::stripEmails('')
         );
 
         $this->assertEquals(
-            'abc',
-            Path::toCamelCase('abc')
+            '',
+            Text::stripEmails('', '[...]')
         );
 
         $this->assertEquals(
-            'goToPath',
-            Path::toCamelCase('go/to/path')
+            '',
+            Text::stripEmails('test@test.de')
         );
 
         $this->assertEquals(
-            'GoToPath',
-            Path::toCamelCase('/go/to/path')
+            '',
+            Text::stripEmails('<test@test.de>')
         );
 
         $this->assertEquals(
-            'aBC',
-            Path::toCamelCase('a/b/c')
+            '<b>[...]</b>',
+            Text::stripEmails('<b>test@test.de</b>', '[...]')
         );
 
         $this->assertEquals(
-            'ABC',
-            Path::toCamelCase('/a/b/c')
+            'Email: [...]',
+            Text::stripEmails('Email: test@test.de', '[...]')
+        );
+
+        $this->assertEquals(
+            'First [...] LAST',
+            Text::stripEmails('First <test@test.de> LAST', '[...]')
+        );
+
+        $this->assertEquals(
+            'First test@test LAST',
+            Text::stripEmails('First test@test LAST', '[...]')
+        );
+
+        $this->assertEquals(
+            'First LAST',
+            Text::stripEmails('First LAST', '[...]')
+        );
+    }
+
+    public function testStripLinks()
+    {
+        $this->assertEquals(
+            '',
+            Text::stripLinks('')
+        );
+
+        $this->assertEquals(
+            '',
+            Text::stripLinks('', '[...]')
+        );
+
+        $this->assertEquals(
+            '',
+            Text::stripLinks('http://test.de')
+        );
+
+        $this->assertEquals(
+            '[...]',
+            Text::stripLinks('//test.de', '[...]')
+        );
+
+        $this->assertEquals(
+            '',
+            Text::stripLinks('www.test.de')
+        );
+
+        $this->assertEquals(
+            ' ',
+            Text::stripLinks(' www.test.de/sub/?lang=en.')
+        );
+
+        $this->assertEquals(
+            'at: ',
+            Text::stripLinks('at: test.de/sub')
+        );
+
+        $this->assertEquals(
+            'Der Link: ',
+            Text::stripLinks('Der Link: www.test.de/test/test.htm?test=1&test2=2')
+        );
+
+        $this->assertEquals(
+            'First [...] LAST',
+            Text::stripLinks('First https://test.de?test LAST', '[...]')
+        );
+
+        $this->assertEquals(
+            '<b>[...]</b>',
+            Text::stripLinks('<b>www.test.de</b>', '[...]')
+        );
+
+        $this->assertEquals(
+            '<b>[...]</b>',
+            Text::stripLinks('<b>//www.test.de</b>', '[...]')
+        );
+
+        $this->assertEquals(
+            '<b>[...]</b>',
+            Text::stripLinks('<b>https://www.test.de</b>', '[...]')
+        );
+    }
+
+
+    public function testStripPhones()
+    {
+        $this->assertEquals(
+            '',
+            Text::stripPhones('')
+        );
+
+        $this->assertEquals(
+            '',
+            Text::stripPhones('', '[...]')
+        );
+
+        $this->assertEquals(
+            '',
+            Text::stripPhones('+49 541 123456')
+        );
+
+        $this->assertEquals(
+            '',
+            Text::stripPhones('0049 (0)541 1234-56')
+        );
+
+        $this->assertEquals(
+            'Tel.: ',
+            Text::stripPhones("Tel.: 0541\t123456")
+        );
+
+        $this->assertEquals(
+            '123 + 123 = 246',
+            Text::stripPhones('123 + 123 = 246')
+        );
+
+        $this->assertEquals(
+            '(+123)',
+            Text::stripPhones('(+123)')
+        );
+
+        $this->assertEquals(
+            'Phone: <b>[...]</b>',
+            Text::stripPhones("Phone: <b>+49 (0) 541 / 123 - 456</b>", '[...]')
+        );
+
+        $this->assertEquals(
+            '9781231231230',
+            Text::stripPhones('9781231231230')
+        );
+
+        $this->assertEquals(
+            '[...]',
+            Text::stripPhones('0123456789012', '[...]')
+        );
+    }
+
+    public function testStripSocials()
+    {
+        $this->assertEquals(
+            '',
+            Text::stripSocials('')
+        );
+
+        $this->assertEquals(
+            '',
+            Text::stripSocials('', '[...]')
+        );
+
+        $this->assertEquals(
+            'test ',
+            Text::stripSocials('test @test')
+        );
+
+        $this->assertEquals(
+            ' ',
+            Text::stripSocials(' @test')
+        );
+
+        $this->assertEquals(
+            'test@test',
+            Text::stripSocials('test@test')
+        );
+
+        $this->assertEquals(
+            'Twitter: [...] etc.',
+            Text::stripSocials('Twitter: @test etc.', '[...]')
+        );
+
+        $this->assertEquals(
+            '',
+            Text::stripSocials('facebook.com/test')
+        );
+
+        $this->assertEquals(
+            '<u>at: </u>[...]<b>now</b>',
+            Text::stripSocials('<u>at: </u>facebook.com/test<b>now</b>', '[...]')
         );
     }
 }
