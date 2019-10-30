bf2d7db86f7b7e0b8d789cefbd903f965de903ce
Ansas Meyer <mail@preigu.com>
2019-10-30 11:49:13 +0100

    Added FoundException class

---
+++ src/Component/Exception/FoundException.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of the PHP components.
+ *
+ * For the full copyright and license information, please view the LICENSE.md file distributed with this source code.
+ *
+ * @license MIT License
+ * @link    https://github.com/ansas/php-component
+ */
+
+namespace Ansas\Slim\Exception;
+
+use Exception;
+
+/**
+ * @author  Ansas Meyer <mail@ansas-meyer.de>
+ */
+class FoundException extends Exception
+{
+    /**
+     * @var mixed
+     */
+    protected $found;
+
+    /**
+     * @param mixed $found [optional]
+     */
+    public function __construct($found)
+    {
+        parent::__construct();
+        $this->found = $found;
+    }
+
+    /**
+     * Get found.
+     *
+     * @return mixed
+     */
+    public function getFound()
+    {
+        return $this->found;
+    }
+}
