79f080e63e0df85932d55b43381c213aacdc6f73
Ansas Meyer <ansas@preigu.com>
2022-06-14 15:00:27 +0200

    LoggerException now extends ContextException

--- src/Component/Exception/ContextException.php
+++ src/Component/Exception/ContextException.php
@@ -6,26 +6,12 @@ use Exception;
 use Throwable;
 
 /**
- * Class ContextException
- *
- * @package Ansas\Component\Exception
  * @author  Ansas Meyer <mail@ansas-meyer.de>
  */
 class ContextException extends Exception
 {
-    /**
-     * @var array Log context
-     */
-    protected $context;
+    protected array $context;
 
-    /**
-     * Create new exception.
-     *
-     * @param string    $message  The Exception message to throw
-     * @param array     $context  Log context [optional]
-     * @param int       $code     The Exception code [optional]
-     * @param Throwable $previous The previous exception used for the exception chaining [optional]
-     */
     public function __construct($message, array $context = [], $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
@@ -33,29 +19,18 @@ class ContextException extends Exception
         $this->context = $context;
     }
 
-    /**
-     * Create new instance.
-     *
-     * @param Exception $e
-     * @param array     $context Log context [optional]
-     *
-     * @return ContextException
-     */
-    public static function createFromException($e, array $context = [])
+    public static function createFromException(Throwable $e, array $context = []): self
     {
         if ($e instanceof self) {
             return $e->addContext($context);
         }
 
-        return new static($e->getMessage(), $context, $e->getCode(), $e->getPrevious());
+        return new self($e->getMessage(), $context, $e->getCode(), $e->getPrevious());
     }
 
     /**
-     * Add log context.
-     *
-     * @param array $context Log context
-     *
-     * @return $this
+     * @return static
+     * @noinspection PhpMissingReturnTypeInspection
      */
     public function addContext(array $context)
     {
@@ -66,12 +41,7 @@ class ContextException extends Exception
         return $this;
     }
 
-    /**
-     * Get log context.
-     *
-     * @return array
-     */
-    public function getContext()
+    public function getContext(): array
     {
         return $this->context;
     }
--- src/Component/Exception/LoggerException.php
+++ src/Component/Exception/LoggerException.php
@@ -2,83 +2,40 @@
 
 namespace Ansas\Component\Exception;
 
-use Exception;
+use Monolog\Logger;
 use Psr\Log\LoggerInterface;
 use Throwable;
 
 /**
- * Class LoggerException
- *
- * @package Ansas\Component\Exception
  * @author  Ansas Meyer <mail@ansas-meyer.de>
  */
-class LoggerException extends Exception
+class LoggerException extends ContextException
 {
     /**
-     * @var mixed Log level
+     * @var mixed
      */
     protected $level;
 
-    /**
-     * @var array Log context
-     */
-    protected $context;
-
-    /**
-     * Create new exception.
-     *
-     * @param string    $message  The Exception message to throw
-     * @param mixed     $level    Log level
-     * @param array     $context  Log context [optional]
-     * @param int       $code     The Exception code [optional]
-     * @param Throwable $previous The previous exception used for the exception chaining [optional]
-     */
+    // TODO change signature to match
     public function __construct($message, $level, array $context = [], int $code = 0, Throwable $previous = null)
     {
-        parent::__construct($message, $code, $previous);
-
-        $this->level   = $level;
-        $this->context = $context;
+        parent::__construct($message, $context, $code, $previous);
+        $this->setLevel($level);
     }
 
-    /**
-     * Create new instance.
-     *
-     * @param Exception $e
-     * @param mixed     $level   Log level
-     * @param array     $context Log context [optional]
-     *
-     * @return LoggerException
-     */
-    public static function createFromException($e, $level, array $context = [])
+    public static function createFromException(Throwable $e, array $context = [], $level = Logger::ERROR): self
     {
+        if ($e instanceof ContextException) {
+            $context = array_merge($e->getContext(), $context);
+        }
         return new static($e->getMessage(), $level, $context, $e->getCode(), $e);
     }
 
     /**
-     * Add log context.
-     *
-     * @param array $context Log context
-     *
-     * @return $this
+     * @return static
+     * @noinspection PhpMissingReturnTypeInspection
      */
-    public function addContext(array $context)
-    {
-        if ($context) {
-            $this->context = array_merge($this->context, $context);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Add exception details to context.
-     *
-     * @param bool $withTrace [optional]
-     *
-     * @return $this
-     */
-    public function addExceptionDetailsToContext($withTrace = false)
+    public function addExceptionDetailsToContext(bool $withTrace = false)
     {
         $this->addContext([
             'exceptionCode' => $this->getCode(),
@@ -94,35 +51,26 @@ class LoggerException extends Exception
     }
 
     /**
-     * Get log context.
-     *
-     * @return array
-     */
-    public function getContext()
-    {
-        return $this->context;
-    }
-
-    /**
-     * Get log level.
-     *
-     * @return string
+     * @return mixed
      */
     public function getLevel()
     {
         return $this->level;
     }
 
-    /**
-     * @param LoggerInterface $logger
-     * @param array           $context Log context [optional]
-     *
-     * @return $this
-     */
     public function log(LoggerInterface $logger, array $context = [])
     {
         $this->addContext($context);
         $logger->log($this->getLevel(), $this->getMessage(), $this->getContext());
+    }
+
+    /**
+     * @return static
+     * @noinspection PhpMissingReturnTypeInspection
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
 
         return $this;
     }
